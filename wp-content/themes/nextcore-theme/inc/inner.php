67c31c3ef16559daf6f30c30c9cfd90dc7e3ad4e
ToanNX <toannx@nextcore.vn>
2026-09-16 13:48:53 +0700

    issues/8901

--- wp-content/themes/nextcore-theme/inc/inner.php
+++ wp-content/themes/nextcore-theme/inc/inner.php
@@ -61,6 +61,9 @@ function nextcore_sidebar_terms($taxonomy, $parent = 0, $terms = null) {
 function nextcore_inner_assets() {
     if (is_front_page()) { return; }
     wp_enqueue_style('nextcore-inner', get_theme_file_uri('/assets/css/inner.css'), array('nextcore-native'), nextcore_asset_version('/assets/css/inner.css'));
+    if (is_page('lien-he')) {
+        wp_enqueue_style('nextcore-contact', get_theme_file_uri('/assets/css/contact.css'), array('nextcore-inner'), nextcore_asset_version('/assets/css/contact.css'));
+    }
     if (is_singular('dich-vu') && get_post_field('post_name', get_queried_object_id()) === 'lark') {
         wp_enqueue_style('nextcore-lark', get_theme_file_uri('/assets/css/lark.css'), array('nextcore-inner'), nextcore_asset_version('/assets/css/lark.css'));
     }
